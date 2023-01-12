9dbe799a31402d287224e6d295fb8adf74ccf10f
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-11 17:46:11 -0800

    font-family and css layout in place

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import './Home.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <h1 className="Gloria">Character Designer</h1>
+    </div>
+  );
 }
