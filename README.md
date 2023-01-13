265b630ed7729aa134bbde5c93e5bcc27891ab77
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-13 14:35:05 -0800

    edited scoreboard and catchphrase display

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Character designer using React
 
+[wireframe](https://whimsical.com/react-character-designer-WAvoMGkKP64haepoy7jKiz)
 ![character designer screenshot](/character-designer.png)
--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -4,8 +4,8 @@ import './Scoreboard.css';
 export default function Scoreboard({ countHead, countMiddle, countPants }) {
   return (
     <div className="Scoreboard">
-      You have changed the character head {countHead} times, the middle {countMiddle} times, and the
-      pants {countPants} times. Pick an outfit already, why dontcha.{' '}
+      You have changed the character&apos;s head <b>{countHead}</b> times, the middle{' '}
+      <b>{countMiddle}</b> times, and the pants <b>{countPants}</b> times.
     </div>
   );
 }
